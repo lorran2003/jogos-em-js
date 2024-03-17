56b712291e1f109a08053b9358ae906664d31de6
lorran2003 <lorran23082003@gmail.com>
2024-03-17 14:36:56 -0300

    jogos em js

--- exercicios de logica/js/forEach.js
+++
@@ -1,7 +0,0 @@
-const array = [1,2,3,4,5];
-array.forEach(function (number, index, array) {
-    console.log(`Numero ${number}, index ${index}, valores do array ${array}`);
-})
-
-let x = array[0];
-console.log(x);
